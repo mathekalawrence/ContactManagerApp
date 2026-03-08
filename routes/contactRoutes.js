dd48c1ceff300b368dfd7f42efa7633e5ffc9f17
Lawrence Muia Matheka <mathekalawrence96@gmail.com>
2026-03-08 16:41:32 +0300

    Professional API getting better

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -2,11 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 router.route("/").get((req, res) => {
-    res.status(200).json({ message: "Get all the contacts" })
+    res.status(200).json({ message: "Get all the contacts" });
 });
 
 router.route("/").post((req, res) => {
-    res.status(200).json({ message: "Create Contact"});
+    res.status(200).json({ message: "Create Contact" });
+});
+
+router.route('/:id').get((req, res) => {
+    res.status(200).json({ message: `Get a contact for ${req.params.id}` });
 });
 
 router.route("/:id").put((req, res) => {
